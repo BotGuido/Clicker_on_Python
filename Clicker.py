4cb0eaf7ec67cdc3a8d66d7b8482e68652531503
Afanasev Sergei <afanasevsergeia@gmail.com>
2023-02-05 18:04:46 +0300

    total

---
+++ Clicker.py
@@ -0,0 +1,33 @@
+# pip install pynput for control mouse and keyboard
+import time
+import threading
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener, KeyCode
+
+toggle_key = KeyCode(char='s') # button s will start programm
+clicking = False
+mouse = Controller()
+
+
+def clicker():
+    while True:
+        if clicking:
+            mouse.click(Button.left, 1)
+            time.sleep(0.1)
+
+def toggle_event(key):
+    if key == toggle_key:
+        global clicking
+        clicking = not clicking
+
+
+def main():
+    clicking_thread = threading.Thread(target=clicker)
+    clicking_thread.start()
+
+    with Listener(on_press=toggle_event) as listener:
+        listener.join()
+
+
+if __name__ == '__main__':
+    main()
